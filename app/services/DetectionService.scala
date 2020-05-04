d83f96fe559f9066b3f49db99f2387806c34a808
Jack Leow <jackgene@gmail.com>
2020-05-04 07:37:59 -0700

    Renamed annotation to annotations.

--- app/services/DetectionService.scala
+++ app/services/DetectionService.scala
@@ -3,7 +3,7 @@ package services
 import java.awt.Rectangle
 import java.awt.image.BufferedImage
 import java.io.{File, FileWriter, PrintWriter}
-import java.nio.file.{Files, Path}
+import java.nio.file.Files
 import java.time.LocalDateTime
 import java.util.Comparator
 
@@ -17,7 +17,6 @@ import org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier
 import play.api.{Configuration, Logging}
 
 import scala.collection.parallel.CollectionConverters._
-import scala.jdk.CollectionConverters._
 import scala.sys.process._
 import scala.util.Try
 
@@ -111,117 +110,116 @@ class DetectionService @Inject()(
   def trainModel(label: String, objectSizePx: Int): Unit = {
     val labelTrainingDir = new File(trainingDir, label)
     if (!labelTrainingDir.exists) labelTrainingDir.mkdirs()
-    val allAnnotations: Seq[(String, Annotations)] = imageService.getAllAnnotationsByPath.toSeq
-
-    // Prepare foreground images and metadata
-    val fgDir = new File(labelTrainingDir, "fg")
-    fgDir.mkdirs()
-    for {
-      (path: String, annotations: Annotations) <- allAnnotations.par
-      openCvPath: String = path.replace(" ", "%20") // OpenCV CLI tools can't handle spaces
-      (annotation: Annotation, idx: Int) <- annotations.annotations.filter(_.label == label).zipWithIndex
-      outFile = new File(fgDir, s"${openCvPath}.${idx}.jpg")
-      if !outFile.exists() || outFile.lastModified < annotations.lastSaved
-      _ = outFile.getParentFile.mkdirs()
-      imgInfoFileWriter = new PrintWriter(new FileWriter(new File(fgDir, s"${openCvPath}.${idx}.info.dat")))
-      img: BufferedImage <- imageService.getImage(path)
-    } {
-      val rect: Rectangle = annotation.shape
-      ImageIO.write(img.getSubimage(rect.x, rect.y, rect.width, rect.height), "jpeg", outFile)
-      try {
-        imgInfoFileWriter.println(
-          s"${outFile.getAbsolutePath} 1 0 0 ${rect.width} ${rect.height}"
-        )
-      } finally {
-        imgInfoFileWriter.close()
-      }
-    }
-    val infoFileWriter = new PrintWriter(new FileWriter(new File(labelTrainingDir, "info.dat")))
-    val numPos: Int =
-      try {
-        Files.walk(fgDir.toPath).iterator.asScala.
-          filter(_.getFileName.toString.endsWith("info.dat")).
-          flatMap { path: Path =>
-            Files.readAllLines(path).asScala
-          }.
-          foldLeft(0) { (count: Int, line: String) =>
-            infoFileWriter.println(line)
-            count + 1
-          }
-      } finally {
-        infoFileWriter.close()
-      }
-
-    // Prepare background images and metadata
-    val bgDir = new File(labelTrainingDir, "bg")
-    bgDir.mkdirs()
-    for {
-      (path: String, annotations: Annotations) <- allAnnotations.par
-      rects: Set[Rectangle] = annotations.annotations.filter(_.label == label).map(_.shape).toSet
-      if rects.nonEmpty
-      outDir = new File(bgDir, path.replace(" ", "%20")) // OpenCV CLI tools can't handle spaces
-      _ = if (outDir.exists && outDir.lastModified < annotations.lastSaved)
-        Files.walk(outDir.toPath).
-        sorted(Comparator.reverseOrder()).
-        forEach(Files.delete _)
-      if !outDir.exists()
-      _ = outDir.mkdirs()
-      img: BufferedImage <- imageService.getImage(path)
-      size: Int = rects.map(_.width).max
-      step: Int = size / 2
-      margin: Int = size * 3 / 4
-      bounds: Rectangle <-
-        if (label == "eye") annotations.annotations.find(_.label == "face").map(_.shape)
-        else Option(img.getRaster.getBounds)
-    } {
-      val imgBgFileWriter = new PrintWriter(new File(outDir, "bg.txt"))
-      try {
-        for {
-          relX: Int <- 0 to (bounds.width - size) by step
-          x = relX + bounds.x
-          relY: Int <- 0 to (bounds.height - size) by step
-          y = relY + bounds.y
-          if rects.forall { rect: Rectangle =>
-            x < rect.x - margin || x >= rect.x + margin || y < rect.y - margin || y >= rect.y + margin
-          }
-        } {
-          val imgOutFile = new File(outDir, f"y${y}%04d_x${x}%04d.jpg")
-          ImageIO.write(img.getSubimage(x, y, size, size), "jpeg", imgOutFile)
-          imgBgFileWriter.println(imgOutFile.getAbsolutePath)
-        }
-      } finally {
-        imgBgFileWriter.close()
-      }
-    }
-    val bgFileWriter = new PrintWriter(new FileWriter(new File(labelTrainingDir, "bg.txt")))
-    val numNeg: Int =
-      try {
-        import scala.jdk.CollectionConverters._
-        Files.walk(bgDir.toPath).iterator.asScala.
-          filter(_.endsWith("bg.txt")).
-          flatMap { path: Path =>
-            Files.readAllLines(path).asScala
-          }.
-          foldLeft(0) { (count: Int, line: String) =>
-            bgFileWriter.println(line)
-            count + 1
-          }
-      } finally {
-        bgFileWriter.close()
-      }
-
     val modelName = s"model_${objectSizePx}x${objectSizePx}_${LocalDateTime.now}"
     val modelDir = new File(labelTrainingDir, modelName)
     new File(modelDir, "data").mkdirs()
 
+    val allPathsAndAnnotations: Seq[(String, Annotations)] = imageService.
+      getAllAnnotationsByPath.
+      filter {
+        case (path: String, _: Annotations) => math.abs(path.hashCode % 5) < 4
+      }.
+      toSeq
+
+    // Prepare foreground images and metadata
+    val infoFile: File = new File(modelDir, "info.dat")
+    val numPos: Int = {
+      val infoFileWriter = new PrintWriter(new FileWriter(infoFile))
+      val fgDir = new File(labelTrainingDir, "fg")
+      fgDir.mkdirs()
+      try {
+        (
+          for {
+            (path: String, annotations: Annotations) <- allPathsAndAnnotations.par
+            (annotation: Annotation, idx: Int) <- annotations.annotations.
+              filter(_.label == label).zipWithIndex
+            img: BufferedImage <- imageService.getImage(path)
+          } yield {
+            val rect: Rectangle = annotation.shape
+            val openCvPath: String = path.replace(" ", "%20") // OpenCV CLI tools can't handle spaces
+            val outFile: File = new File(fgDir, s"${openCvPath}.${idx}.jpg")
+
+            if (!outFile.exists() || outFile.lastModified < annotations.lastSaved) {
+              outFile.getParentFile.mkdirs()
+              ImageIO.write(
+                img.getSubimage(rect.x, rect.y, rect.width, rect.height), "jpeg", outFile
+              )
+            }
+            infoFileWriter.println(
+              s"${outFile.getAbsolutePath} 1 0 0 ${rect.width} ${rect.height}"
+            )
+          }
+        ).size
+      } finally {
+        infoFileWriter.close()
+      }
+    }
+
+    // Prepare background images and metadata
+    val bgFile: File = new File(modelDir, "bg.txt")
+    val numNeg: Int = {
+      val bgFileWriter = new PrintWriter(new FileWriter(bgFile))
+      val bgDir = new File(labelTrainingDir, "bg")
+      bgDir.mkdirs()
+      try {
+        (
+          for {
+            (path: String, annotations: Annotations) <- allPathsAndAnnotations.par
+            rects: Set[Rectangle] = annotations.annotations.filter(_.label == label).map(_.shape).toSet
+            if rects.nonEmpty
+            img: BufferedImage <- imageService.getImage(path)
+            bounds: Rectangle <-
+              if (label == "eye") annotations.annotations.find(_.label == "face").map(_.shape)
+              else Option(img.getRaster.getBounds)
+          } yield {
+            val outDir: File = new File(bgDir, path.replace(" ", "%20")) // OpenCV CLI tools can't handle spaces
+            val size: Int = rects.map(_.width).max
+            val step: Int = size / 2
+            val margin: Int = size * 3 / 4
+            val negs: Seq[(Int,Int,File)] =
+              for {
+                relX: Int <- 0 to (bounds.width - size) by step
+                x = relX + bounds.x
+                relY: Int <- 0 to (bounds.height - size) by step
+                y = relY + bounds.y
+                if rects.forall { rect: Rectangle =>
+                  x < rect.x - margin || x >= rect.x + margin || y < rect.y - margin || y >= rect.y + margin
+                }
+              } yield {
+                val imgOutFile: File = new File(outDir, f"y${y}%04d_x${x}%04d.jpg")
+                bgFileWriter.println(imgOutFile.getAbsolutePath)
+                (x, y, imgOutFile)
+              }
+
+            if (!outDir.exists || outDir.lastModified < annotations.lastSaved) {
+              if (!outDir.exists) outDir.mkdirs()
+              else
+                Files.walk(outDir.toPath).
+                  sorted(Comparator.reverseOrder()).
+                  forEach(Files.delete _)
+
+              for ((x: Int, y: Int, imgOutFile: File) <- negs) {
+                ImageIO.write(img.getSubimage(x, y, size, size), "jpeg", imgOutFile)
+              }
+            }
+
+            negs.size
+          }
+        ).sum
+      } finally {
+        bgFileWriter.close()
+      }
+    }
+
+    // Create positive samples
     val vecCmd =
       s"${openCvBinDir}/opencv_createsamples " +
       s"-info info.dat " +
-      s"-vec ${new File(modelDir, "positive.vec").getAbsolutePath} " +
+      s"-vec positive.vec " +
       s"-num ${numPos} " +
       s"-w ${objectSizePx} -h ${objectSizePx}"
     val vecLogFile = new File(modelDir, "opencv_createsamples.log")
-    runWithLogging(vecCmd, labelTrainingDir, vecLogFile)
+    runWithLogging(vecCmd, modelDir, vecLogFile)
 
     val numStages = 20
     val minHitRate = 0.995
@@ -232,14 +230,14 @@ class DetectionService @Inject()(
       map { numPosTrain: Int =>
         val trainCmd =
           s"${openCvBinDir}/opencv_traincascade " +
-          s"-data ${new File(modelName, "data").getPath} " +
-          s"-vec ${new File(modelName, "positive.vec").getPath} " +
-          "-bg bg.txt " +
+          s"-data data " +
+          s"-vec positive.vec " +
+          s"-bg bg.txt " +
           s"-numPos ${numPosTrain} -numNeg ${numNeg} -numStages ${numStages} " +
           s"-w ${objectSizePx} -h ${objectSizePx} -minHitRate ${minHitRate} " +
           "-baseFormatSave " + // Save in old format as required by CUDA classifier
           openCvTrainCascadeOpts
-        runWithLogging(trainCmd, labelTrainingDir, trainLogFile)
+        runWithLogging(trainCmd, modelDir, trainLogFile)
       }.
       takeWhile(_ != 0).
       size
